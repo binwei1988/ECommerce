fc5da13586bf63e07c303651ec2e8a5bb3a9f288
binwei1988 <binwei1988@gatech.edu>
2021-09-17 23:46:21 -0400

    update category

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.category;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,46 @@ public class CategoryService {
 	private CategoryRepository repo;
 	
 	public List<Category> listAll() {
-		return (List<Category>) repo.findAll();
+		List<Category> rootCategories = repo.findRootCategories();
+		return listHierarchicalCategories(rootCategories);
+	}
+	
+	private List<Category> listHierarchicalCategories(List<Category> rootCategories) {
+		List<Category> hierarchicalCategories = new ArrayList<>();
+		
+		for (Category rootCategory : rootCategories) {
+			hierarchicalCategories.add(Category.copyFull(rootCategory));
+			
+			Set<Category> children = rootCategory.getChildren();
+			
+			for (Category subCategory : children) {
+				String name = "--" + subCategory.getName();
+				hierarchicalCategories.add(Category.copyFull(subCategory, name));
+				
+				listSubHierarchicalCategories(hierarchicalCategories, subCategory, 1);
+			}
+		}
+		
+		return hierarchicalCategories;
+	}
+	
+	private void listSubHierarchicalCategories(List<Category> hierarchicalCategories,
+			Category parent, int subLevel) {
+		Set<Category> children = parent.getChildren();
+		int newSubLevel = subLevel + 1;
+		
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {				
+				name += "--";
+			}
+			name += subCategory.getName();
+		
+			hierarchicalCategories.add(Category.copyFull(subCategory, name));
+			
+			listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel);
+		}
+		
 	}
 	
 	public Category save(Category category) {
@@ -37,7 +77,7 @@ public class CategoryService {
 					String name = "--" + subCategory.getName();
 					categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 					
-					listChildren(categoriesUsedInForm, subCategory, 1);
+					listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, 1);
 				}
 			}
 		}		
@@ -45,7 +85,8 @@ public class CategoryService {
 		return categoriesUsedInForm;
 	}
 	
-	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+	private void listSubCategoriesUsedInForm(List<Category> categoriesUsedInForm, 
+			Category parent, int subLevel) {
 		int newSubLevel = subLevel + 1;
 		Set<Category> children = parent.getChildren();
 		
@@ -58,7 +99,15 @@ public class CategoryService {
 			
 			categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 			
-			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+			listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
 		}		
 	}	
+	
+	public Category get(Integer id) throws CategoryNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (NoSuchElementException ex) {
+			throw new CategoryNotFoundException("Could not find any category with ID " + id);
+		}
+	}
 }
