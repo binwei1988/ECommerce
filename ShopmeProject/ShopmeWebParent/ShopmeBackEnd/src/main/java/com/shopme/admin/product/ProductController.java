16da9d87736d4ad313ad634c0a755c48fee63742
binwei1988 <binwei1988@gatech.edu>
2021-09-20 23:44:21 -0400

    save, delete & productImage

--- ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
+++ ShopmeProject/ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/product/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.brand.BrandService;
 import com.shopme.common.entity.Brand;
@@ -42,11 +44,37 @@ public class ProductController {
 	}
 	
 	@PostMapping("/products/save")
-	public String saveProduct(Product product) {
-		System.out.println("Product Name: " + product.getName());
-		System.out.println("Brand ID: " + product.getBrand().getId());
-		System.out.println("Category ID: " + product.getCategory().getId());
+	public String saveProduct(Product product, RedirectAttributes ra) {
+		productService.save(product);
+		ra.addFlashAttribute("message", "The product has been saved successfully.");
 		
 		return "redirect:/products";
 	}
+	
+	@GetMapping("/products/{id}/enabled/{status}")
+	public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
+			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
+		productService.updateProductEnabledStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The Product ID " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
+		
+		return "redirect:/products";
+	}	
+	
+	@GetMapping("/products/delete/{id}")
+	public String deleteProduct(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			productService.delete(id);
+			
+			redirectAttributes.addFlashAttribute("message", 
+					"The product ID " + id + " has been deleted successfully");
+		} catch (ProductNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("message", ex.getMessage());
+		}
+		
+		return "redirect:/products";
+	}	
 }
